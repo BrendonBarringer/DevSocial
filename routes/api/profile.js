434892a5a0e7cfe96e8dbfcb2e7dbef29ba94fab
Brendon <brendonbarringer@gmail.com>
2018-11-07 20:53:37 -0600

    created post route for profile.js and validation for profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
+
+// Load Validation
+const validateProfileInput = require("../../validation/profile");
+
 //const keys = require('../../config/keys');
 
 // Load Profile Model
@@ -42,6 +46,14 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
     // Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
