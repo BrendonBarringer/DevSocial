f3eae00decf5416b3023f7e969648d3274201ce0
Brendon <brendonbarringer@gmail.com>
2019-11-25 11:15:14 -0600

    DevSocial

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -46,7 +46,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className='navbar bg-dark'>
       <h1>
         <Link to='/'>
-          <i className='fas fa-code' /> DevConnector
+          <i className='fas fa-code' /> DevSocial
         </Link>
       </h1>
       {!loading && (
@@ -65,7 +65,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(
-  mapStateToProps,
-  { logout }
-)(Navbar);
+export default connect(mapStateToProps, { logout })(Navbar);
